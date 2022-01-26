ff028b8bf3645f9803c74b5939747606dee5bb76
Gonzalo Campos <ecampos@etsy.com>
2022-01-25 20:17:05 -0600

    Finish stored properties section.

--- TheBasics.playground/Pages/StoredProperties.xcplaygroundpage/Contents.swift
+++ TheBasics.playground/Pages/StoredProperties.xcplaygroundpage/Contents.swift
@@ -1,7 +1,318 @@
-//: [Previous](@previous)
 
-import Foundation
+struct FixedLengthRange {
+    var firstValue: Int
+    let length: Int
+}
 
-var greeting = "Hello, playground"
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+rangeOfThreeItems.firstValue = 6
 
-//: [Next](@next)
+let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
+
+// rangeOfFourItems.firstValue = 6 --> error: it is a constant
+
+class DataImporter {
+    var filename = "data.text"
+}
+
+class DataManager {
+    lazy var importer = DataImporter()
+    var data: [String] = []
+}
+
+let manager = DataManager()
+manager.data.append("Some data")
+manager.data.append("Some more data")
+// at this point, manager.importer hasn't yet been created
+
+print(manager.importer.filename) // now it was created
+
+// computed properties
+struct Point {
+    var x = 0.0
+    var y = 0.0
+}
+struct Size  {
+    var width  = 0.0
+    var height = 0.0
+}
+
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        // shorthand getter: if there's just one expression, return can be omitted
+        get {
+            Point(x: origin.x + (size.width / 2),
+                  y: origin.y + (size.height / 2))
+        }
+        // shorthand setter: no parameter, default newValue binding
+        set {
+            origin.x = newValue.x - (size.width / 2)
+            origin.y = newValue.y - (size.height / 2)
+        }
+    }
+}
+
+var square = Rect(origin: Point(x: 0.0, y: 0.0),
+                  size: Size(width: 10.0, height: 10.0))
+let initialSquareCenter = square.center
+square.center = Point(x: 15.0, y: 15.0)
+
+print("square.origin is now at (\(square.origin.x), \(square.origin.y))")
+
+// read-only computed property
+struct Cuboid {
+    var width  = 0.0
+    var height = 0.0
+    var depth  = 0.0
+    var volume: Double {
+        width * height * depth
+    }
+}
+
+// Cuboid(width: 10, height: 10, depth: 10).volume --> error: volume is a get-only property
+
+// property observers
+class StepCounter {
+    var totalSteps: Int = 0 {
+        willSet(newTotalSteps) {
+            print("About to set toalSteps to \(newTotalSteps)")
+        }
+        didSet {
+            if totalSteps > oldValue {
+                print("Added \(totalSteps - oldValue) steps")
+            }
+        }
+    }
+}
+
+let stepCounter = StepCounter()
+stepCounter.totalSteps = 200
+stepCounter.totalSteps = 360
+stepCounter.totalSteps = 896
+
+// property wrappers
+@propertyWrapper
+struct TwelveOrLess {
+    private var number = 0
+    var wrappedValue: Int {
+        get { number }
+        set { number = min(newValue, 12) }
+    }
+}
+
+struct SmallRectangle {
+//    @TwelveOrLess var height: Int
+//    @TwelveOrLess var width: Int
+    
+    // eqvuivalent code
+    private var _height = TwelveOrLess()
+    private var _width = TwelveOrLess()
+    var height: Int {
+        get { return _height.wrappedValue }
+        set { _height.wrappedValue = newValue }
+    }
+    var width: Int {
+        get { return _width.wrappedValue }
+        set { _width.wrappedValue = newValue }
+    }
+
+}
+
+var rectangle = SmallRectangle()
+print(rectangle.height)
+
+rectangle.height = 10
+print(rectangle.height)
+
+rectangle.height = 24
+print(rectangle.height)
+
+// setting initial values for wrapped properties
+@propertyWrapper
+struct SmallNumber {
+    private var maximum: Int
+    private var number: Int
+    
+    var wrappedValue: Int {
+        get { number }
+        set { number = min(newValue, maximum) }
+    }
+    
+    init() {
+        maximum = 12; number = 0
+    }
+    
+    init(wrappedValue: Int) {
+        maximum = 12; number = min(wrappedValue, maximum)
+    }
+    
+    init(wrappedValue: Int, maximum: Int) {
+        self.maximum = maximum; number = min(wrappedValue, maximum)
+    }
+}
+
+struct ZeroRectangle {
+    @SmallNumber var height: Int
+    @SmallNumber var width: Int
+}
+
+var zeroRectangle = ZeroRectangle()
+print(zeroRectangle.height, zeroRectangle.width)
+
+struct UnitRectangle {
+    @SmallNumber var height: Int = 1
+    @SmallNumber var width: Int = 1
+}
+
+var unitRectangle = UnitRectangle()
+print(unitRectangle.height, unitRectangle.width)
+
+struct NarrowRectangle {
+    @SmallNumber(wrappedValue: 2, maximum: 5) var height: Int
+    @SmallNumber(wrappedValue: 3, maximum: 4) var width: Int
+}
+
+var narrowRectangle = NarrowRectangle()
+print(narrowRectangle.height, narrowRectangle.width)
+
+narrowRectangle.height = 100
+narrowRectangle.width = 100
+print(narrowRectangle.height, narrowRectangle.width)
+
+struct MixedRectangle {
+    @SmallNumber var height: Int = 1
+    @SmallNumber(maximum: 9) var width: Int = 2
+}
+
+var mixedRectangle = MixedRectangle()
+print(mixedRectangle.height)
+
+mixedRectangle.height = 20
+print(mixedRectangle.height)
+
+
+@propertyWrapper
+struct SmallNumber2 {
+    private var number: Int
+    private(set) var projectedValue: Bool
+    
+    var wrappedValue: Int {
+        get { number }
+        set {
+            if newValue > 12 {
+                number = 12
+                projectedValue = true
+            } else {
+                number = newValue
+                projectedValue = false
+            }
+        }
+    }
+    
+    init() {
+        self.number = 0
+        self.projectedValue = false
+    }
+}
+
+struct SomeStructure {
+    @SmallNumber2 var someNumber: Int
+}
+var someStructure = SomeStructure()
+
+someStructure.someNumber = 4
+print(someStructure.$someNumber)
+
+someStructure.someNumber = 55
+print(someStructure.$someNumber)
+
+enum _Size {
+    case small
+    case large
+}
+
+struct SizedRectangle {
+    @SmallNumber2 var height: Int
+    @SmallNumber2 var width: Int
+    
+    mutating func resize(to size: _Size) -> Bool {
+        switch size {
+        case .small:
+            height = 10
+            width = 20
+        case .large:
+            height = 100
+            width = 100
+        }
+        return $height || $width
+        
+    }
+}
+
+//func someFunction() {
+//    @SmallNumber var myNumber: Int = 0
+//
+//    myNumber = 10
+//    myNumber = 24
+//}
+
+struct SomeNewStructure {
+    static var storedTypeProperty = "Some value."
+    static var computerTypeProperty: Int {
+        return 1
+    }
+}
+enum SomeEnumeration {
+    static var storedTypeProperty = "Some value."
+    static var computerTypeProperty: Int {
+        return 6
+    }
+}
+class SomeClass {
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 27
+    }
+    class var overrideableComputerTypeProperty: Int {
+        return 107
+    }
+}
+
+print(SomeNewStructure.storedTypeProperty)
+SomeNewStructure.storedTypeProperty = "Another value."
+print(SomeNewStructure.storedTypeProperty)
+
+print(SomeEnumeration.computerTypeProperty)
+print(SomeClass.computedTypeProperty)
+print(SomeClass.overrideableComputerTypeProperty)
+
+struct AudioChannel {
+    static let thresholdLevel = 10
+    static var maxInputLevelForAllChannels = 0
+    var currentLevel: Int = 0 {
+        didSet {
+            if currentLevel > AudioChannel.thresholdLevel {
+                // cap the new audio level to threshold level
+                currentLevel = AudioChannel.thresholdLevel
+            }
+            if currentLevel > AudioChannel.maxInputLevelForAllChannels {
+                // store as the new overall maximum input level
+                AudioChannel.maxInputLevelForAllChannels = currentLevel
+            }
+        }
+    }
+}
+
+var leftChannel = AudioChannel()
+var rightChannel = AudioChannel()
+
+leftChannel.currentLevel = 7
+print()
+print(leftChannel.currentLevel)
+print(AudioChannel.maxInputLevelForAllChannels)
+
+rightChannel.currentLevel = 11
+print(rightChannel.currentLevel)
+print(AudioChannel.maxInputLevelForAllChannels)
